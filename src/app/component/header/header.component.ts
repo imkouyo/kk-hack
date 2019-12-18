fa4b7691eb82c9bb76e9034c28e984d2b802b9ff
hunterko <hunter@hunterkode-MacBook-Pro.local>
2019-12-18 14:31:57 +0800

    v14

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  faSearch = faSearch
   constructor() { }
 
   ngOnInit() {
